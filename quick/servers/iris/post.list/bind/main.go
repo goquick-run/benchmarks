5b6efec72baf8d3b9e47f4857f9e3e327ad3e143
ArturR4chid <arturrachid123@gmail.com>
2025-05-02 10:50:33 -0300

    docs/adding-readme

--- quick/servers/iris/post.list/bind/main.go
+++ quick/servers/iris/post.list/bind/main.go
@@ -1,6 +1,6 @@
 package main
 
-// import (
+ import (
  	"encoding/json"
  	"log"
  	"github.com/kataras/iris/v12"
--- quick/servers/iris/post.simple/byte/main.go
+++ quick/servers/iris/post.simple/byte/main.go
@@ -1,7 +1,7 @@
 package main
 
-// import (
-// 	"log"
+ import (
+	"log"
  	"github.com/kataras/iris/v12"
  )
  // $ curl --location 'http://localhost:8080/v1/user' \
